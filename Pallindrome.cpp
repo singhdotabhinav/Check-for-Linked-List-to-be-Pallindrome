459bc83dd3e5e00fb8f17358c0209d6ac8919cc4
Abhinav kumar <42245122+singhdotabhinav@users.noreply.github.com>
2020-08-07 13:27:34 +0530

    Create Pallindrome.cpp

---
+++ Pallindrome.cpp
@@ -0,0 +1,36 @@
+//Step 1:Traverse through the list and push on to stack
+//Step 2:Traverse through list again and compare between stack top and start of linked list
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        
+        stack<int> s;
+        ListNode *temp=head;
+        while(temp!=NULL){
+            s.push(temp->val);
+            temp=temp->next;
+        }
+        temp=head;
+        while(!s.empty()&&temp!=NULL){
+            int num=s.top();
+            s.pop();
+            if(num!=temp->val){
+                return false;
+            }
+            temp=temp->next;
+              
+        } 
+         return true;
+    }
+};
